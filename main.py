7be40a723b6369c98d9bbbc060eaa83614917649
SohanH2025 <sohan.hormuzdiar2025@gbwl.org>
2024-12-02 14:13:52 +0000

    dhddhdh

--- main.py
+++ main.py
@@ -1,5 +1,27 @@
+#function for each day
+def day01():
+    print("day01")
+    #read txt file
+    file = open('day01-input.txt','r')
+    #data = file.read()
+    #print(data)
+
+    left = []
+    right = []
+
+    split = [line.strip()for line in file]
+    for line in split:
+        left.append(line.split("   ")[0])
+        right.append(line.split("   ")[1])
+    
+    sorted_left = sorted(left)
+    sorted_right = sorted(right)
+    for (l,r) in zip(sorted_left, sorted_right):
+        print(l,r)
+
+
 def main():
-    print("hello world")
+    day01() #change function for each day
 
 
 if __name__ == "__main__":
